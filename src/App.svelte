d5e3a02df5711ed8cb6a24ea03b3d5f8df85ef1c
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-12-17 15:48:54 +0530

    ADD: basic styling

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<style>
+    :global(body) {
+        font-family: sans-serif;
+        background: radial-gradient(
+            circle at top right,
+            #7a5cff 0%,
+            rgb(38, 108, 247) 20%,
+            #4780eb 35%,
+            #93e4ff 100%
+        );
+        color: #333;
+        font-weight: 550;
+    }
+</style>
